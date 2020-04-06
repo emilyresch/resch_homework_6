4f6e313f2f3420e93d5a88374e46064f4f14151a
Emily Resch <eresch527@gmail.com>
2020-04-06 18:38:45 -0500

    added structure of html

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //HW 6///
 
 //BASIC REQUIREMENTS
-
-//begin with form - input space for name of city and a submit button. 
+//begin with form - input space for name of city and a submit button, and a "last searched" area (search history from local storage)
 //Layout will include form on one half of page (html) and appended results/info 
 //on other half via js
+//open weather key - 69f2b66e6c9854ab3784dbb114768257
+
+
 
 //FILES REQUIRED
 //will need files for html, js and css 
@@ -21,24 +23,21 @@
 //do an ajax search call on whatever is typed into search bar 
 //form validation
 //make ajax request - traverse response to find information we need 
+//append CURRENT button and FUTURE button - both will need similar onclick events 
+//depending on which the user clicks, find appropriate information: 
     //key info - name, date, *weather icon*, temp, humidity, wind speed, UV index
     //weather icons - potentially in an array based on weather conditions
     //UV index - will determine color displayed (favorable, moderate, severe)
 //append correct information to html 
-//save search to localStorage as value within a history object
-//append search history to page (as clickable buttons)
+//save search to localStorage as value within a history object (we will need this for the search history and for the "last searched")
+//append search history to page (as clickable buttons) 
+    //show current and future weather conditions for that city
+
 
 //CSS FILE
 
 //ACCEPTANCE CRITERIA
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
+
 // THEN I am again presented with current and future conditions for that city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
