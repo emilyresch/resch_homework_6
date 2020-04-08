9fde34d8abf94656a3822ba8320dc5cfc1b2e84f
Emily Resch <eresch527@gmail.com>
2020-04-07 19:01:15 -0500

    current weather nearly done, working on future weather

--- script.js
+++ script.js
@@ -41,35 +41,44 @@
 //search button onclick event
 $(document).ready(function () {
 
-    //making current and future buttons
+    //empty array for searched cities
+    var searched = [];
     var currentBtn = $("<button>").attr("id", "current-btn");
-    currentBtn.text("See Current Weather");
-    currentBtn.attr("class", "btn btn-primary");
     var futureBtn = $("<button>").attr("id", "future-btn");
-    futureBtn.text("See Future Forecast");
-    futureBtn.attr("class", "btn btn-primary");
     var forecast = $(".weather-info");
-    forecast.append(currentBtn);
-    forecast.append(futureBtn);
+    var current = $("<div>").attr("class", "current-weather");
+    var future = $("<div>").attr("class", "future-weather");
+    forecast.append(current);
+    forecast.append(future);
 
     //grab user input value
 
     $("#search-button").on("click", function (e) {
         e.preventDefault();
         var city = $("#city-input").val().trim();
+        searched.push(city);
         console.log(city);
+        console.log(searched);
+        //making current and future buttons
+
+        currentBtn.text("See Current Weather");
+        currentBtn.attr("class", "btn btn-primary");
+
+        futureBtn.text("See Future Forecast");
+        futureBtn.attr("class", "btn btn-primary");
+
+        current.append(currentBtn);
+        current.append(futureBtn);
+        $("#current-btn").on("click", currentWeather);
+        $("#future-btn").on("click", futureWeather);
+
 
     })
-
-
-
     //need to validate - make sure city is correct and also not numbers etc
     //search button needs an onclick event that will create two buttons inside of the forecast div - current and future
 
     //need a current button and future button onclick event listener
 
-    $("#current-btn").on("click", currentWeather);
-    $("#future-btn").on("click", futureWeather);
 
 
     //global scope variables: weather images array, search input, 
@@ -82,27 +91,97 @@ $(document).ready(function () {
     //make ajax call and then function() {
     //inside of function: 
     function currentWeather() {
-
         var city = $("#city-input").val().trim();
         var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=69f2b66e6c9854ab3784dbb114768257";
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response.name);
-            var temp = ((response.main.temp - 273.15)*1.8) + 32; //need to convert to farenheit
-            console.log(temp.toFixed(2)); 
+            console.log(response);
+            var country = response.sys.country;
+            console.log(response.weather[0].icon);
+            var temp = ((response.main.temp - 273.15) * 1.8) + 32; //need to convert to farenheit
+            console.log(temp.toFixed(2));
             console.log(response.main.humidity);
             console.log(response.wind.speed);
+            //name
+            var nameTitle = $("<h2>");
+            current.append(nameTitle);
+            nameTitle.text(response.name + ", " + country);
+
+            // function addSearchHistory() {
+            //     // add it to search history too as a button
+            //     forecast.empty();
+            //     var srchHist = $(".search-history");
+            //     var cityNameBtn = $("<button>");
+            //     cityNameBtn.text(response.name);
+            //     srchHist.prepend(cityNameBtn);
+            //     cityNameBtn.on("click", currentWeather);
+            // }
+
+            //icon 
+            var icon = response.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            var imageIcon = $("<img>").attr("src", iconURL);
+            current.append(imageIcon);
+            //temp
+            var tempH = $("<h3>");
+            current.append(tempH);
+            tempH.text(temp.toFixed(2) + " degrees F");
+            //humidity
+            var humH = $("<h3>");
+            current.append(humH);
+            humH.text(response.main.humidity + " per cent humidity");
+            //windspeed
+            var windH = $("<h3>");
+            current.append(windH);
+            windH.text(response.wind.speed + " mph winds");
+            //uv index
 
 
         })
+
 
     }
 
 
     function futureWeather() {
+        // console.log("future weather!");
+        var city = $("#city-input").val().trim();
+        var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=69f2b66e6c9854ab3784dbb114768257";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            console.log((((response.list[7].main.temp - 273.15) * 1.8) + 32).toFixed(2));
+            console.log((((response.list[14].main.temp - 273.15) * 1.8) + 32).toFixed(2));
+            console.log((((response.list[21].main.temp - 273.15) * 1.8) + 32).toFixed(2));
+            console.log((((response.list[28].main.temp - 273.15) * 1.8) + 32).toFixed(2));
+            console.log((((response.list[35].main.temp - 273.15) * 1.8) + 32).toFixed(2));
 
+            for (var i=7; i<=35; i+=7) {
+            var icon = response.list[i].weather[0].icon;
+            console.log(icon);
+            var iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+            var iconFuture = $("<img>").attr("src", iconURL);
+            }
+
+
+            // var temp = ((response.main.temp - 273.15) * 1.8) + 32; //need to convert to farenheit
+            // $(".current-weather").empty();
+
+
+
+        })
+    }
+    
+    function clearForecast () {
+        if (current) {
+        future.empty();
+        }else{
+        current.empty();
+        }
     }
     //console.log/test out the object response
     //find information needed in CURRENT forecast
